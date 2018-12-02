a1fbe9afd73c427ece922414852b27399546679d
Dima Laba <dimalaba@Dimas-MacBook-Pro.local>
2018-12-02 17:21:32 +0200

    Add delta action

--- crypto-search/actions.js
+++ crypto-search/actions.js
@@ -1,3 +1,19 @@
-import axios
+const axios = require('axios');
 
-const baseLink = 'http://localhost:8082/'
+const baseLink = 'http://localhost:8082/';
+
+const actions = {
+  delta: 'delta',
+  keygen: 'keygen',
+};
+
+const delta = async (k1, k2) => {
+  const formData = new FormData();
+
+  formData.append('action', actions.delta);
+  formData.append('k1', k1);
+  formData.append('k2', k2);
+
+  const result = await axios.get(baseLink, formData);
+  return result.data;
+};
