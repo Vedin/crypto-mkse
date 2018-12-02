e8a893c2f7c6d59e61e3e10f8616800ab0273915
Dima Laba <dimalaba@Dimas-MacBook-Pro.local>
2018-12-02 17:30:41 +0200

    Restructure

--- crypto-search/actions.js
+++ crypto-search/actions.js
@@ -9,42 +9,27 @@ const actions = {
   adjust: 'adjust',
 };
 
-const keygen = async () => {
+const argsToFormData = (args) => {
   const formData = new FormData();
-  formData.append('action', actions.keygen);
-  const result = await axios.get(baseLink, formData);
+
+  Object.keys(args).forEach((arg) => {
+    formData.append(arg, args[arg]);
+  });
+
+  return formData;
+};
+
+const baseRequest = async (args) => {
+  const result = await axios.get(baseLink, argsToFormData(args));
   return result.data;
 };
 
-const delta = async (k1, k2) => {
-  const formData = new FormData();
+const keygen = async () => baseRequest({ action: actions.keygen });
 
-  formData.append('action', actions.delta);
-  formData.append('k1', k1);
-  formData.append('k2', k2);
+const delta = async (k1, k2) => baseRequest({ action: actions.delta, k1, k2 });
 
-  const result = await axios.get(baseLink, formData);
-  return result.data;
-};
+const token = async (k, word) =>
+  baseRequest({ action: actions.token, k, word });
 
-const token = async (k, word) => {
-  const formData = new FormData();
-
-  formData.append('action', actions.token);
-  formData.append('k1', k);
-  formData.append('k2', word);
-
-  const result = await axios.get(baseLink, formData);
-  return result.data;
-};
-
-const adjust = async (tok, delta) => {
-  const formData = new FormData();
-
-  formData.append('action', actions.adjust);
-  formData.append('tok', tok);
-  formData.append('delta', delta);
-
-  const result = await axios.get(baseLink, formData);
-  return result.data;
-};
+const adjust = async (tok, delta) =>
+  baseRequest({ action: actions.adjust, tok, delta });
