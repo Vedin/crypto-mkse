3b041e847c80bdbcce83414df5af2c8266ec4d7e
Denys <d.tsomenko@dzento.com>
2018-12-03 00:37:17 +0200

    Fix connection bug

--- crypto-search/model.js
+++ crypto-search/model.js
@@ -52,7 +52,7 @@ Meteor.startup(function () {
 /* trusted IDP: */
 //var idp_pub = '8a7fe03431b5fc2db3923a2ab6d1a5ddf35cd64aea35e743' +
 //              'ded7655f0dc7e085858eeec06e1c7da58c509d57da56dbe6';
-//idp_init("http://localhost:3000", idp_pub, false);
+//idp_init("http://localhost:8040", idp_pub, false);
 
 // use IDP only if active attacker
 Accounts.config({sendVerificationEmail: active_attacker()});
