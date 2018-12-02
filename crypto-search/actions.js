39f7b588cb22bfc89c363b0b42693b76eeb0d1c8
Dima Laba <dimalaba@Dimas-MacBook-Pro.local>
2018-12-02 17:23:27 +0200

    Add Token action

--- crypto-search/actions.js
+++ crypto-search/actions.js
@@ -3,8 +3,9 @@ const axios = require('axios');
 const baseLink = 'http://localhost:8082/';
 
 const actions = {
-  delta: 'delta',
   keygen: 'keygen',
+  delta: 'delta',
+  token: 'token',
 };
 
 const keygen = async () => {
@@ -20,6 +21,17 @@ const delta = async (k1, k2) => {
   formData.append('action', actions.delta);
   formData.append('k1', k1);
   formData.append('k2', k2);
+
+  const result = await axios.get(baseLink, formData);
+  return result.data;
+};
+
+const token = async (k, word) => {
+  const formData = new FormData();
+
+  formData.append('action', actions.token);
+  formData.append('k1', k);
+  formData.append('k2', word);
 
   const result = await axios.get(baseLink, formData);
   return result.data;
